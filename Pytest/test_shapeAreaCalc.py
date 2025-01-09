80705c078e1c6ca8b1b7189d12eb99174fb1f1fa
milnerlewis <lewismilner0808@gmail.com>
2025-01-09 11:42:46 +0000

    updated code

--- Pytest/test_shapeAreaCalc.py
+++ Pytest/test_shapeAreaCalc.py
@@ -65,23 +65,28 @@ def test_circle():
     assert circle(5) == 78.5
     assert circle(10) == 314
     assert circle(0) == 0
+    assert circle("potato") == False
 
 def test_square():
     assert square(5) == 25
     assert square(10) == 100
     assert square(0) == 0
+    assert square("potato") == False
 
 def test_rectangle():
     assert rectangle(5, 10) == 50
     assert rectangle(10, 10) == 100
     assert rectangle(1,0) == 0
+    assert rectangle("potato", 10) == False
 
 def test_parollelogram():
     assert parollelogram(5, 10) == 50
     assert parollelogram(10, 10) == 100
     assert parollelogram(1,0) == 0
+    assert parollelogram("potato", 10) == False
 
 def test_triangle():
     assert triangle(5, 10) == 25
     assert triangle(10, 10) == 50
     assert triangle(1, 0) == 0
+    assert triangle("potato", 10) == False
